94a672bb83d826b799a7d6ccac0d142ea39bad21
Metsuryuu <qjroman@gmail.com>
2021-03-09 13:46:15 -0500

    Starting Template Engines

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import express from 'express'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
+// * This is what makes this an Express app.
 const app = express()
+app.set('view engine', 'ejs')
 
+const directory = dirname(fileURLToPath(import.meta.url))
+
+// Easily make HTTP requests using the app you made above.
 app.get('/', (req, res) => {
-    res.send('This is the home page.')
+    res.sendFile(`${directory}/index.html`)
 })
 
 app.get('/contact', (req, res) => {
-    res.send('This is the contact page.')
+    res.sendFile(`${directory}/contact.html`)
+})
+
+// * To grab a URL parameter, use the request object. You use the params object, then the field.
+app.get('/profile/:id', (req, res) => {
+    res.send(`You requested to see a profile with the id of ${req.params.id}`)
 })
 
 app.listen(3000)
