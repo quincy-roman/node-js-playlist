b0d9f3908dc67c76bdc655362db273f7d42568f4
Metsuryuu <qjroman@gmail.com>
2021-03-09 22:01:04 -0500

    Handling POST Requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import bodyParser from 'body-parser'
 
 // * This is what makes this an Express app.
 const app = express()
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs')
 app.use('/assets', express.static('assets'))    // * Express's way of serving static files.
 
 const directory = dirname(fileURLToPath(import.meta.url))
+const urlEncodedParser = bodyParser.urlencoded({ extended: false})  // ! Find better version
 
 // Easily make HTTP requests using the app you made above.
 app.get('/', (req, res) => {
@@ -24,6 +26,12 @@ app.get('/contact', (req, res) => {
 app.get('/profile/:name', (req, res) => {
     let data = {age: 16, job: 'Student Council President', hobbies: ['Mapo Tofu', 'Maintaining order', 'Helping others']}
     res.render('profile', {person: req.params.name, data: data})
+})
+
+// * Making a POST request requires a middleware parser.
+app.post('/contact', urlEncodedParser, (req, res) => {
+    console.log(req.body)
+    res.render('contact-success', {data: req.body})
 })
 
 app.listen(3000)
